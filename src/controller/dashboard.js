058c7ca2d00fb8313ba2726fe5f13d2b2087dfd5
sombir5 <sombirverma5@gmail.com>
2022-12-20 15:22:06 +0530

    MOck13

--- src/controller/dashboard.js
+++ src/controller/dashboard.js
@@ -4,11 +4,8 @@ const allPlayer = async (req, res) => {
   let { page = 1, limit = 10 } = req.query;
   try {
     let players = await User.find({})
-      .skip((page - 1) * limit)
-      .limit(limit)
-    let nos = await User.find().count();
-    console.log(nos);
-    return res.send({ data: players, totalPages: Math.ceil(nos / limit) });
+    
+    return res.send({ data: players });
   } catch (error) {
     console.log(error.message);
   }
